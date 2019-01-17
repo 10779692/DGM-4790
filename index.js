a908fee2a57d5d6a271407127682c77285b6f5ca
Jared Reed <42556502+10779692@users.noreply.github.com>
2019-01-16 17:58:07 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.setHeader('Content-type', 'text/html')
+        res.setHeader('Jared', 'Jared is awesome')
+        res.write('<html>')
+        res.write('<head><title>Testing My Server</title></head>')
+        res.write('<body><h1>Welcome to My Server Page</h1><br><img src="https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy.gif"></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (req.url === '/api') {
+        const bear = {
+            animal: 'Grizzly Bear',
+            color: 'Brown',
+            weight: 1200,
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify(bear))
+        return res.end()
+    }
+    if (req.url === 'user') {
+        const body =[];
+        req.on('data', chunk => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody.split('=')[1])
+        })
+        req.statusCode = 302
+        req.setHeader('location', '/')
+        req.end()
+    }
+})
+
+server.listen(5000)
+
+/*
+Your server will do the following:
+
+Listen on port 5000
+Have a default route that returns HTML with a message welcoming you to the page
+Have a '/api' route that returns a JSON object containing at least 3 key/value pairs of data
+Ensure your response headers are all set properly
+*/
