9ed27cad6f1b523edfefeaff0a94fe70e300e0ca
Jared Reed <42556502+10779692@users.noreply.github.com>
2019-01-16 18:07:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,12 +36,3 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(5000)
-
-/*
-Your server will do the following:
-
-Listen on port 5000
-Have a default route that returns HTML with a message welcoming you to the page
-Have a '/api' route that returns a JSON object containing at least 3 key/value pairs of data
-Ensure your response headers are all set properly
-*/
